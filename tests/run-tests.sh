c2fb0feb8a41cd8bf6e80aafb9ea5122741c205c
EchoBit3 <cassot.jean02@gmail.com>
2026-09-24 04:18:29 -0300

    test: suite de seguridad whitebox

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,7 +5,7 @@ set -u
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 RUN_ALL_FAIL=0
 
-for t in blackbox_setup.test.sh blackbox_launcher.test.sh blackbox_uninstall.test.sh whitebox_content.test.sh blackbox_update.test.sh; do
+for t in blackbox_setup.test.sh blackbox_launcher.test.sh blackbox_uninstall.test.sh whitebox_content.test.sh blackbox_update.test.sh whitebox_security.test.sh; do
     printf "\n>>> %s\n" "$t"
     if bash "$SCRIPT_DIR/$t"; then
         :
